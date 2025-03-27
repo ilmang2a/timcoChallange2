29b2fcdbdae53c4f9aa45c8e5b3d1345270fc32e
Ilman Hamzatov <ilman.hamzatov@witzenmann.com>
2025-03-27 13:14:56 +0000

    post request

--- CSVHelper.cs
+++ CSVHelper.cs
@@ -8,68 +8,29 @@ using System.Globalization;
 using timcoChallange2.Models;
 using System.Text;
 using System.IO;
+using System.Text.Json;
 
 namespace CSVHelper
 {
     public class CSVHelper
     {
         private readonly ILogger<CSVHelper> _logger;
-        private readonly List<Person> people;
 
 
 
         public CSVHelper(ILogger<CSVHelper> logger)
         {
-            people = new List<Person>();
 
             _logger = logger;
-            people.Add(new Person { Name = "Ilman", Surename = "Hamzatov", Email = "ilman@mail.de" });
-            people.Add(new Person { Name = "Donald", Surename = "Duck", Email = "donald@mail.de" });
-            people.Add(new Person { Name = "Trump", Surename = "Donald", Email = "trump@mail.de" });
-            people.Add(new Person { Name = "Che", Surename = "Guevera", Email = "che@mail.de" });
+
 
         }
 
         [Function("CSVHelper")]
-        public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequestData req)
+        public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "Post")] HttpRequestData req)
         {
-
-
+            var data = await JsonSerializer.DeserializeAsync<Person>(req.Body);
             var response = req.CreateResponse(System.Net.HttpStatusCode.OK);
-            response.Headers.Add("Content-Type", "text/csv");
-            response.Headers.Add("Content-Disposition", "attachment; filename=\"people.csv\"");
-
-
-            var memoryStream = new MemoryStream();
-
-
-            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
-            {
-                HasHeaderRecord = true,
-            };
-
-            try
-            {
-                using (var streamWriter = new StreamWriter(memoryStream, Encoding.UTF8, 1024, leaveOpen: true))
-
-                using (var csv = new CsvWriter(streamWriter, CultureInfo.InvariantCulture))
-                {
-                    csv.WriteRecords(people);
-                    await streamWriter.FlushAsync();
-
-                }
-                memoryStream.Position = 0;
-
-
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError($"Failed to write CSV file: {ex.Message}");
-                throw;
-            }
-
-
-            await memoryStream.CopyToAsync(response.Body);
             return response;
         }
     }
